f80f958d685f4c904a7446a088217909a710578d
baulhpthiag <lailapmhga@gmail.com>
2020-03-13 16:31:50 +0800

    修改IniDemo

--- src/IniDemoTemp/IniDemoMainWidget.cpp
+++ src/IniDemoTemp/IniDemoMainWidget.cpp
@@ -13,16 +13,22 @@ IniDemoMainWidget::~IniDemoMainWidget()
 
 void IniDemoMainWidget::test01ButtonClicked()
 {
+	//如果文件不存在，会自动创建文件
+
 	QString applicationDir = qApp->applicationDirPath();
 	QString iniPath = applicationDir + "/testIni.ini";
 	QSettings settings(iniPath, QSettings::IniFormat);
 
 	settings.setValue("Student/Name", "tom");
 	settings.setValue("Student/Age", 1);
-
+	settings.setValue("Student/isXXX",true);
+	settings.setValue("Student/high", 1.70);
 	settings.setValue("School/Name", "xxxSchool");
 	settings.setValue("School/Pos", "1");
+	
 
+	bool b = settings.value("Student/isXXX").toBool();
+	double high = settings.value("Student/high").toDouble();
 
 
 }
