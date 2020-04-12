5f5111cf82dc7b1d376cba757f5ed456a1650042
alpha <sallypha@163.com>
2020-04-12 22:23:29 +0800

    topNavigation

--- src/UI_TopNavigation/MainWidget.cpp
+++ src/UI_TopNavigation/MainWidget.cpp
@@ -1,14 +1,9 @@
 #include "MainWidget.h"
 #include "StyleSheetHelper.h"
-#include <QApplication>
-
 
 MainWidget::MainWidget(QWidget *parent)
 	: QWidget(parent)
 {
 	ui.setupUi(this);
-	StyleSheetHelper::loadStyleSheet(this, qApp->applicationDirPath() + "/alphaStyleSheetGray.css");
-	
+	StyleSheetHelper::loadStyleSheet(this, qApp->applicationDirPath() + "/topNavigation.css");
 }
-
-
